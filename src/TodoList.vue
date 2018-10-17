7989be4bc2e704db9c4aaae4053154315e34cb3b
Jingyi21 <qianwumeng_2cyndi@qq.com>
2018-10-17 18:19:43 +0800

    Initial commit

---
+++ src/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="inputValue"/>
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item 
+        v-for="(item,index) of list"
+        :content="item"
+        :key="index"
+        :index="index"
+        @delete="handleDelete"
+      ></todo-item>
+    </ul>
+  </div>  
+</template>
+
+<script>
+import TodoItem from './components/Todoitem'
+
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit(){
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li @click="handleDelete">
+        {{ content }}
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['content','index'],
+    methods: {
+        handleDelete(){
+            this.$emit('delete',this.index)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
