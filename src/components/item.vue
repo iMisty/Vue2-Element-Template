4f4202b1b6f7d3dc6d2e2ea6087435977fcd4f13
Miya <qianwumeng_2cyndi@qq.com>
2018-10-19 01:56:00 +0800

    Update src/components/item.vue

---
+++ src/components/item.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="['todo-item', todo.completed ? 'completed' : '']">
+        <input 
+            type="checkbox"
+            class="toggle"
+            v-model="todo.completed"
+        >
+        <label>{{todo.content}}</label>
+        <button class="destory" @click="deleteTodo"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        deleteTodo(){
+            this.$emit('del',this.todo.id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
