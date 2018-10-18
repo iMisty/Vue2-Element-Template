3f74b14bec009557a8acb2f4238d56c9fc697c65
Miya <qianwumeng_2cyndi@qq.com>
2018-10-19 01:55:57 +0800

    Update src/components/footer.vue

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <span>Written by Miya-Misty</span>
+    </footer>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    footer{
+    margin: 0 auto;
+    bottom: 0;
+    width: 100%;
+    }
+</style>
