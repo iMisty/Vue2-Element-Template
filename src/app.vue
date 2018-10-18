6658a061892d798770d637698e43c931ead86b51
Miya <qianwumeng_2cyndi@qq.com>
2018-10-19 01:55:55 +0800

    Update src/app.vue

---
+++ src/app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Todo></Todo>
+    <Footer></Footer>
+  </div>  
+</template>
+
+<script>
+import header from './components/header'
+import todo from './components/todo'
+import TodoItem from './components/item'
+import footer from './components/footer'
+
+export default {
+  components: {
+    'Header': header,
+    'todo-item': TodoItem,
+    'Todo': todo,
+    'Footer': footer
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit(){
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete(index){
+      this.list.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  height: 100vh;
+  text-align: center;
+}
+</style>
