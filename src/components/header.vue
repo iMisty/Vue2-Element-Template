da44595b215c417b98f86bcefb61c5ba91acae15
Miya <qianwumeng_2cyndi@qq.com>
2018-10-19 01:55:58 +0800

    Update src/components/header.vue

---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="main-header">
+        <h1>ToDo</h1>
+    </header>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    .main-header{
+        padding: 16vh 0;
+    }
+</style>
