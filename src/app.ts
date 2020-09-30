770c5d22a433372fa86ae93a12acbc91b2e33496
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-09-30 12:21:20 +0800

    Update: 跨域支持

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-28 17:24:53
- * @LastEditTime: 2020-09-29 18:31:29
+ * @LastEditTime: 2020-09-30 11:59:33
  * @LastEditors: Miya
  * @Description: Main
  * @FilePath: \Single-Search-API\src\app.ts
@@ -9,17 +9,19 @@
  */
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as cors from 'koa2-cors';
 import { SearchController } from './controller';
 import bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
+app.use(cors());
 app.use(bodyParser());
 app.use(router.routes());
 
 router.get('/', SearchController.getSearchData);
 
-app.listen(12450);
+app.listen(12451);
 
 console.log('APP running');
