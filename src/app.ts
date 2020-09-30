e0bdd5fabdfa141c0ee7cba689724ea333224de3
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-09-30 16:22:30 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-28 17:24:53
- * @LastEditTime: 2020-09-30 14:57:38
+ * @LastEditTime: 2020-09-30 16:13:31
  * @LastEditors: Miya
  * @Description: Main
  * @FilePath: \Single-Search-API\src\app.ts
@@ -10,13 +10,20 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as cors from 'koa2-cors';
-import * as bodyParser from 'koa-bodyparser';
+import bodyParser = require('koa-bodyparser');
 import { SearchController } from './controller';
 
 const app = new Koa();
 const router = new Router();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // 允许跨域的地址，我的理解类似白名单，*代表全部允许
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 暴露header列表
+    allowMethods: ['GET'], // 请求方式
+    allowHeaders: ['Accept', 'Origin', 'Content-type', 'Authorization'],
+  })
+);
 app.use(bodyParser());
 app.use(router.routes());
 
