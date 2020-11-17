32421e8430f2595bb4dcc2abb06265700fc5b88f
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-11-17 18:21:01 +0800

    WIP: 接口增删改查

--- app.ts
+++ app.ts
@@ -1,39 +1,36 @@
 /*
  * @Author: Miya
  * @Date: 2020-09-28 17:24:53
- * @LastEditTime: 2020-11-16 16:04:02
+ * @LastEditTime: 2020-11-17 15:09:14
  * @LastEditors: Miya
  * @Description: Main
  * @FilePath: \Single-Search-API\src\app.ts
  * @Version: 1.0
  */
 import * as Koa from 'koa';
-import * as Router from 'koa-router';
 import * as cors from 'koa2-cors';
+import * as Mongoose from 'mongoose';
 import bodyParser = require('koa-bodyparser');
-import { SearchController } from './controller/search';
-import { Install } from './controller/install';
+import Router from './router/index';
+const dbConfig = require('./config/db');
 
 const app = new Koa();
-const router = new Router();
 
 app.use(
   cors({
     origin: '*', // 允许跨域的地址，我的理解类似白名单，*代表全部允许
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 暴露header列表
-    allowMethods: ['GET'], // 请求方式
+    allowMethods: ['GET','POST','PUT','DELETE'], // 请求方式
     allowHeaders: ['Accept', 'Origin', 'Content-type', 'Authorization'],
   })
 );
 app.use(bodyParser());
-app.use(router.routes());
+app.use(Router());
 
-router.get('/', (ctx) => {
-  ctx.body = 'Hello TypeScript';
+Mongoose.connect(dbConfig.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-router.get('/search', SearchController.getSearchData);
-router.get('/t', Install.install);
 
 app.listen(12451);
 
--- src/db/db.ts
+++
@@ -1,2 +0,0 @@
-const db = 'mongodb://1:22@localhost:27017/mdb_search';
-    module.exports={ db };
