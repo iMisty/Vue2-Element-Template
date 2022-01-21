515e256886b01cb5b00ea7b5797cb47dcf91a321
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-01-22 00:17:57 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import styleImport from "vite-plugin-style-import";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    styleImport({
+      libs: [
+        {
+          libraryName: "element-plus",
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            return `element-plus/theme-chalk/${name}.css`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        },
+      ],
+    }),
+  ],
+  base: './', 
+  server: {
+    port: 12450,
+  },
+  resolve:{
+    alias:{
+      '@': resolve(__dirname,'/src')
+    }
+  }
+});
