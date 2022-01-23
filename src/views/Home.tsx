5be5e32be79b7df3af5e5d4aba2ac6a0e0864d65
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-01-24 00:18:13 +0800

    Create Home.tsx

---
+++ src/views/Home.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Miya
+ * @Date: 2022-01-23 23:34:58
+ * @LastEditTime: 2022-01-23 23:34:59
+ * @LastEditors: Miya
+ * @Description: Home Page
+ * @FilePath: \Misty\src\views\Home.tsx
+ */
+import { defineComponent } from 'vue';
+
+const Home = defineComponent({
+  name: 'Home',
+  render() {
+    return (
+      <div>Home</div>
+    );
+  },
+});
+
+export default Home;
