21fa864ad08f781d622a56d5bd3a6bfd2c3b603c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-01-24 00:18:15 +0800

    Create About.tsx

---
+++ src/views/About.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Miya
+ * @Date: 2022-01-23 23:35:29
+ * @LastEditTime: 2022-01-24 00:08:33
+ * @LastEditors: Miya
+ * @Description: About Page
+ * @FilePath: \Misty\src\views\About.tsx
+ */
+import { defineComponent } from 'vue';
+
+const About = defineComponent({
+  name: 'About',
+  render() {
+    return (
+      <el-button>22</el-button>
+    );
+  },
+});
+
+export default About;
