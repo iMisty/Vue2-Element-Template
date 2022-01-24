88552838841b1dcea854c010e31e76d12c076934
Kagura Koeri <lin.miya.moe@gmail.com>
2022-01-24 17:58:27 +0800

    Create SVGIcon.tsx

---
+++ src/components/SVGIcon.tsx
@@ -0,0 +1,21 @@
+/*
+ * @Description: SVG Icon Component
+ * @Version: 1.0
+ * @Author: Mirage
+ * @Date: 2022-01-24 17:49:28
+ * @LastEditors: Mirage
+ * @LastEditTime: 2022-01-24 17:49:28
+ */
+import { defineComponent,defineProps } from 'vue';
+
+const SVGIcon = defineComponent({
+  name: 'SVGIcon',
+  render() {
+    return (
+      // TODO
+      <div></div>
+    );
+  },
+});
+
+export default SVGIcon;
