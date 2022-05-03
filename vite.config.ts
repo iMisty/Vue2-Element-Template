3868b68c6a63cb96c9c4e5d5a4c1fc3852521fe9
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-05-04 02:59:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Miya
+ * @Date: 2022-01-23 22:54:12
+ * @LastEditTime: 2022-02-09 22:03:54
+ * @LastEditors: Miya
+ * @Description: Vite config
+ * @FilePath: \Misty\vite.config.ts
+ */
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -8,21 +16,21 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    styleImport({
-      libs: [
-        {
-          libraryName: "element-plus",
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            return `element-plus/theme-chalk/${name}.css`;
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`;
-          },
-        },
-      ],
-    }),
+    // styleImport({
+    //   libs: [
+    //     {
+    //       libraryName: "element-plus",
+    //       esModule: true,
+    //       ensureStyleFile: true,
+    //       resolveStyle: (name) => {
+    //         return `element-plus/theme-chalk/${name}.css`;
+    //       },
+    //       resolveComponent: (name) => {
+    //         return `element-plus/lib/${name}`;
+    //       },
+    //     },
+    //   ],
+    // }),
   ],
   base: './', 
   server: {
